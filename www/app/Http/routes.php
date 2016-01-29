1e49383a54144a2c1f1c533329665d817dc6dc6d
Timothy Zhou <timothyzhou@vip.qq.com>
2016-01-29 17:50:28 +0700

    make a simple tasks

--- www/app/Http/routes.php
+++ www/app/Http/routes.php
@@ -2,30 +2,60 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you will register all of the routes in an application.
+| Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks', [
+    	'tasks' => Task::orderBy('created_at', 'asc')->get()
+    ]);
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
 
-Route::group(['middleware' => ['web']], function () {
-    //
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+
+	return redirect('/');
 });
+
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{id}', function ($id) {
+	Task::findOrFail($id)->delete();
+
+	return redirect('/');
+});
+
+/**
+ * About
+ */
+Route::get('about', 'PagesController@about');
